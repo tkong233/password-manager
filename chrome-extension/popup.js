8dbddb5f2cf3024358118d7a1a5090f514f2d114
tkong233 <tkong@wesleyan.edu>
2018-10-31 04:24:45 -0400

    set up password, username in popup

---
+++ chrome-extension/popup.js
@@ -0,0 +1,7 @@
+const password = document.getElementById('password').value;
+const username = document.getElementById('username').value;
+const website = document.getElementById('website');
+
+console.log(password)
+console.log(username)
+console.log(website)
